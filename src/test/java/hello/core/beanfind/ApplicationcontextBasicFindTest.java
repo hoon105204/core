695f69711e245f553f9ac0d2ef8c755526e74ed3
hoon105204 <hoon105204@gmail.com>
2023-02-20 17:58:16 +0900

    update

--- src/test/java/hello/core/beanfind/ApplicationcontextBasicFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationcontextBasicFindTest.java
@@ -6,7 +6,11 @@ import hello.core.member.MemberServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ApplicationcontextBasicFindTest {
 
@@ -16,7 +20,35 @@ public class ApplicationcontextBasicFindTest {
     @DisplayName("빈 이름으로 조회")
     void findBeanByName() {
         MemberService memberService = ac.getBean("memberService", MemberService.class);
-        Assertions.assertEquals(MemberServiceImpl.class , memberService);
+        System.out.println("memberService = " + memberService);
+        System.out.println("memberService.getClass() = " + memberService.getClass());
 
+        assertInstanceOf(MemberServiceImpl.class, memberService);
     }
+
+    @Test
+    @DisplayName("이름 없이 타입으로 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertInstanceOf(MemberServiceImpl.class, memberService);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertInstanceOf(MemberServiceImpl.class, memberService);
+        // 구체적으로 적는 것은 좋지 않음, 역할에 의존해야 함
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 X")
+    void findBeanByNameX() {
+        // 이름으로 조회시 예외 발생해야 할 경우
+//        MemberService xxxx = ac.getBean("XXXX", MemberService.class);
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("XXXX", MemberService.class));
+    }
+
+
 }
